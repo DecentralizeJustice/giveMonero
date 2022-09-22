1dcbcc7980e517f267d820415e980072a96ea437
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-09-21 21:38:49 -0400

    Working Vite Site

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,35 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+    <div class="p-fluid">
+        <div class="card">
+            <h5>Basic</h5>
+            <Chips v-model="value1" />
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+            <h5>Comma Separator</h5>
+            <Chips v-model="value2" separator="," />
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+            <h5>Template</h5>
+            <Chips v-model="value3">
+                <template #chip="slotProps">
+                    <div>
+                        <span>{{slotProps.value}} - (active) </span>
+                        <i class="pi pi-user-plus" style="font-size: 14px"></i>
+                    </div>
+                </template>
+            </Chips>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const value1 = ref();
+        const value2 = ref();
+        const value3 = ref();
+
+        return { value1, value2, value3 }
+    }
 }
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Chips from 'primevue/chips';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('Chips', Chips);
+
+app.mount('#app');
